9d3260b435e3a01b3dc010d5b4a902317e47b234
itsjustyuji <vedantsharmavs2002@gmail.com>
2026-06-07 18:57:19 +0530

    Updated code with object variables. Part 1.3 done

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ const Part=(props)=>{
 const Content=(props)=>{
   return(
      <div>
-      <Part part={props.p1} exercises={props.ex1}/>
-      <Part part={props.p2} exercises={props.ex2}/>
-      <Part part={props.p3} exercises={props.ex3}/>
+      <Part part={props.p1.name} exercises={props.p1.exercises}/>
+      <Part part={props.p2.name} exercises={props.p2.exercises}/>
+      <Part part={props.p3.name} exercises={props.p3.exercises}/>
     </div>
   )
 }
@@ -32,21 +32,27 @@ const Total=(props)=>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
       <Content 
-      p1={part1} ex1={exercises1}
-      p2={part2} ex2={exercises2} 
-      p3={part3} ex3={exercises3} 
+      p1={part1}
+      p2={part2} 
+      p3={part3} 
       />
-      <Total ex1={exercises1} ex2={exercises2}  ex3={exercises3} />
+      <Total ex1={part1.exercises} ex2={part2.exercises}  ex3={part3.exercises} />
     </div>
   )
 }
